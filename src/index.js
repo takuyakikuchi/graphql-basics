d6caaa4c53be3e0110e20d77cd7d15e5188e195b
Takuya Kikuchi <kikuchi.takuya.dbce8@gmail.com>
2020-04-11 12:13:46 +0900

    added import export

--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
-console.log("Hello GraphQL");
+import add, { subtract } from "./math";
+
+console.log(`Default export "add": ${add(1, 2)}`);
+console.log(`Named export "subtract": ${subtract(1, 2)}`);
