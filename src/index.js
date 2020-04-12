a01f1f694476d8dc57fe7173ef53ac04d9df5711
Takuya Kikuchi <kikuchi.takuya.dbce8@gmail.com>
2020-04-12 19:30:04 +0900

    moved schema file and updated nodemon setting

--- src/index.js
+++ src/index.js
@@ -88,71 +88,6 @@ let comments = [
   },
 ];
 
-// ----------- Type definition(Schema) -----------
-const typeDefs = `
-  type Query {
-    comment: Comment!
-    post: Post!
-    user: User!
-    comments: [Comment!]!
-    posts(query: String): [Post!]!
-    users(query: String): [User!]!
-  }
-
-  type Mutation {
-    createUser(data: CreateUserInput!): User!
-    deleteUser(id: ID!): User!
-    createPost(data: CreatePostInput!): Post!
-    deletePost(id: ID!): Post!
-    createComment(data: CreateCommentInput!): Comment!
-    deleteComment(id: ID!): Comment!
-  }
-
-  input CreateUserInput {
-    name: String!
-    email: String!
-    age: Int
-  }
-
-  input CreatePostInput {
-    title: String!
-    body: String!
-    published: Boolean!
-    author: ID!
-  }
-
-  input CreateCommentInput {
-    text: String!
-    author: ID!
-    post: ID!
-  }
-
-  type Comment {
-    id: ID!
-    text: String!
-    author: User!
-    post: Post!
-  }
-
-  type Post {
-    id: ID!
-    title: String!
-    body: String!
-    published: Boolean!
-    author: User!
-    comments: [Comment!]!
-  }
-
-  type User {
-    id: ID!
-    name: String!
-    email: String!
-    age: Int
-    posts: [Post!]!
-    comments: [Comment!]!
-  }
-`;
-
 // ------------- Resolver(function) ----------------
 const resolvers = {
   Query: {
@@ -343,7 +278,7 @@ const resolvers = {
 
 // -------------- Server -----------------
 const server = new GraphQLServer({
-  typeDefs,
+  typeDefs: "./src/schema.graphql",
   resolvers,
 });
 
